61faf06ed1e3ce9c1b45f064f67ba366387b506b
Chandraprakash <sahuprakash.er@gmail.com>
2023-07-23 11:01:27 +0530

    Browser Router to HashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
